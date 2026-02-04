6bea38a49d2a7c885d49145ea6046b43a05423f0
AiranCana <airancc2005@gmail.com>
2026-02-04 16:16:22 +0100

    edit algo

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,32 @@
 /*   By: acanadil <acanadil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 09:44:31 by acanadil          #+#    #+#             */
-/*   Updated: 2026/02/03 15:57:08 by acanadil         ###   ########.fr       */
+/*   Updated: 2026/02/04 16:15:50 by acanadil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*reader(char *buf)
+{
+	int	rbuf;
+
+	rbuf = 1;
+	while (rbuf > 0)
+	{
+		
+	}
+	return (buf);
+}
 
 char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*buffer;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0))
 		return (NULL);
+	buffer = reader(buffer);
 	if (!buffer)
 		return (NULL);
 	return (line);
