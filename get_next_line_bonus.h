1718906064786f692aa2e447c5f0dace50cf1f8a
AiranCana <airancc2005@gmail.com>
2026-02-09 14:55:48 +0000

    save all

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acanadil <acanadil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/03 13:11:18 by acanadil          #+#    #+#             */
-/*   Updated: 2026/02/09 10:59:21 by acanadil         ###   ########.fr       */
+/*   Created: 2026/02/09 10:37:47 by acanadil          #+#    #+#             */
+/*   Updated: 2026/02/09 10:37:50 by acanadil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
